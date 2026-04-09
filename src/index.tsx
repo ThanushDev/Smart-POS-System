b91f8c36bbd28cb0aafc8c3550e641a7b49401b7
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-09 22:51:38 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css'; // දැන් මෙය src ඇතුළේ ඇති නිසා ./ ලෙස තිබීම නිවැරදියි
+import './styles.css'; 
 
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
