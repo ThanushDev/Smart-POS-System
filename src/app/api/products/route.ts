9ff425622d0d9ed943b87a17f46fe315672fb572
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-04-09 19:17:03 +0530

    Update route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,17 +2,22 @@ import { NextResponse } from 'next/server';
 import { connectToDatabase } from '@/lib/mongodb';
 import Product from '@/models/Product';
 
-// Inventory (GET)
 export async function GET() {
   await connectToDatabase();
-  const products = await Product.find({});
+  const products = await Product.find({}).sort({ createdAt: -1 });
   return NextResponse.json(products);
 }
 
-// (POST)
-export async function POST(request: Request) {
+export async function POST(req: Request) {
   await connectToDatabase();
-  const data = await request.json();
-  const newProduct = await Product.create(data);
-  return NextResponse.json(newProduct);
+  const data = await req.json();
+  const product = await Product.create(data);
+  return NextResponse.json(product);
+}
+
+export async function DELETE(req: Request) {
+  const { id } = await req.json();
+  await connectToDatabase();
+  await Product.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Product Deleted" });
 }
